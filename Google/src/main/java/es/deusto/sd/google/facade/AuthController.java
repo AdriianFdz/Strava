b4ddf5f295966289777344c8c5462ae280ac8e35
Adrian Fernandez <adrian.f@opendeusto.es>
2024-11-28 14:35:26 +0100

    add: conection between Strava and Google service

--- Google/src/main/java/es/deusto/sd/google/facade/AuthController.java
+++ Google/src/main/java/es/deusto/sd/google/facade/AuthController.java
@@ -1,7 +1,5 @@
 package es.deusto.sd.google.facade;
 
-import java.util.Optional;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
