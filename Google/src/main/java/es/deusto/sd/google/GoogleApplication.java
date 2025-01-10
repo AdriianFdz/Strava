52b0c107c2bc8b5b8a9ee8b97b3269ca02c95210
Adrian Fernandez <adrian.f@opendeusto.es>
2025-01-10 13:47:11 +0100

    fix: versions

--- Google/src/main/java/es/deusto/sd/google/GoogleApplication.java
+++ Google/src/main/java/es/deusto/sd/google/GoogleApplication.java
@@ -3,13 +3,10 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import es.deusto.sd.google.entity.Credentials;
-
 @SpringBootApplication
 public class GoogleApplication {
 
 	public static void main(String[] args) {
-		Credentials c = new Credentials("a","a");
 		SpringApplication.run(GoogleApplication.class, args);
 	}
 }
